067d30f259a98b744edd150d9383c90ef61b2525
trapcodeio <trapcode.io@gmail.com>
2022-12-18 15:12:01 +0100

    fix a few bugs

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import {init, __dirname} from "@xpresser/framework/index.js";
+import {init, __dirname} from "@xpresser/framework";
 import {RegisterServerModule} from "@xpresser/server-module";
 import ExpressProvider from "./index.js";
 
@@ -8,11 +8,11 @@ const base = __dirname(import.meta.url);
 // Init Xpresser
 const $ = await init({
     env: "development",
-    name: "Xpresser Express Provider",
+    name: "Express Provider",
     debug: {
         bootCycle: {
-            started: true,
-            completed: true,
+            // started: true,
+            // completed: true,
         }
     },
     paths: {
@@ -26,5 +26,5 @@ const express = new ExpressProvider();
 // Register Server Module with Express Provider
 await RegisterServerModule($, express);
 
-$.start().catch((e) => $.console.logErrorAndExit(e));
+$.start().catch((e:any) => $.console.logErrorAndExit(e));
 
